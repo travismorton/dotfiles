f89b32c86ef369b68d31e39ec2b8a0f89a2a40a9
Travis Morton <tmorton@republicwireless.com>
2020-12-12 23:06:17 -0500

    Added initialization script

---
+++ run_install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Packages needed and not installed by this script:
+#       neovim (needs to be built from source if on ubuntu)
+#       fish
+#       fzf
+
+#####################
+# Powerline Install #
+#####################
+pip3 install --user powerline-status)
+
+USER_PATH=/home/travis/
+
+################
+# Neovim setup #
+################
+mkdir -p $USER_PATH.local/share/nvim/
+mkdir -p $USER_PATH.config/nvim
+python3 -m venv $USER_PATH.local/share/nvim/venv
+
+# Install python neovim package for python support
+$USER_PATH.local/share/nvim/venv/bin/python3 -m pip install neovim
+
+# Install vim-plug
+curl -fLo $USER_PATH.local/share/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install and update if there were changes
+nvim --headless +PlugInstall +qa
+nvim --headless +PlugUpdate +qa
